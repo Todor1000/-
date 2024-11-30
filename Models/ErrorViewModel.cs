24b617d43de029f7f8042a20a0ce5ab70ab15f52
pc1_10 <pc1_10@DESKTOP-QD5J6D0>
2024-11-30 09:04:39 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Локален_Бюлетински_Помошник.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
